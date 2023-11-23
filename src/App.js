8480e2fa7106553179c71d310ea345193efd6de9
buttmoosa174 <133306901+buttmoosa174@users.noreply.github.com>
2023-11-23 09:21:13 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './style.css';
+import {Login} from './components/Login';
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Home } from '@mui/icons-material';
+import Register from './components/Register';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+
+const App = () => {
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route
+        path="*"
+        element={<ProtectedRoutes><Home /></ProtectedRoutes>}
+      />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+  )
 }
+
+// export default function App() {
+//   return (
+//     <div>
+//     <Router>
+//     <Routes>
+//       <Route
+//         path="/"
+//         element={<ProtectedRoutes><Home /></ProtectedRoutes>}
+//       />
+//       <Route path="/register" element={<Register />} />
+//       <Route path="/login" element={<Login />} />
+//     </Routes>
+//   </Router>
+//   </div>
+//   );
+// }
+
+export function ProtectedRoutes(props) {
+  if (localStorage.getItem("user")) {
+    return props.children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
+
+export default App;
+
+
